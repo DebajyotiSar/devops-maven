d2128b4f65c5bd607402ed347027939976f3a891
Debajyoti <debajyotisarkar1198@gmail.com>
2020-09-02 15:02:32 +0000

    Updated the Source code file

--- src/main/java/com/cdmproject/helloworld/Helloworld.java
+++ src/main/java/com/cdmproject/helloworld/Helloworld.java
@@ -1,11 +1,9 @@
 package com.cdmproject.helloworld;
 
 public class Helloworld {
+
 	public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello,Welcome to Devops Project!");
-=======
-        System.out.println("Hello, Welcome to Devops project!");
->>>>>>> ea87d85a8eb71e46e737943366dc3256b74851a0
+        System.out.println("Welcome to Devops Project and learning!");
+        
 		}
 }
