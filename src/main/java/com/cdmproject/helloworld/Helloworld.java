8044efb6da988aeaef8d610ee05c967324e640de
Debajyoti <debajyotisarkar1198@gmail.com>
2020-09-02 15:36:12 +0000

    Updated Helloworld.java

--- src/main/java/com/cdmproject/helloworld/Helloworld.java
+++ src/main/java/com/cdmproject/helloworld/Helloworld.java
@@ -4,7 +4,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
         System.out.println("Welcome to Devops learning!");
-	System.out.println("This is for Github-webhooks testing")
+	System.out.println("This is for Github-webhooks testing");
         
 		}
 }
