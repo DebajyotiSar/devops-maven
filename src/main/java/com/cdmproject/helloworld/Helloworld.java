121f972d0592bd959eff3ed9dd72e2ae517cbc5c
Debajyoti <debajyotisarkar1198@gmail.com>
2020-09-02 15:23:58 +0000

    Updated Hellworld file

--- src/main/java/com/cdmproject/helloworld/Helloworld.java
+++ src/main/java/com/cdmproject/helloworld/Helloworld.java
@@ -3,7 +3,7 @@ package com.cdmproject.helloworld;
 public class Helloworld {
 
 	public static void main(String[] args) {
-        System.out.println("Welcome to Devops Project and learning!");
+        System.out.println("Welcome to Devops learning!");
 	System.out.println("This is for Github-webhooks testing")
         
 		}
