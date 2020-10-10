2a3fee294a4c4a99658bc33b1d6f64ed8053ab94
Debajyoti <debajyotisarkar1198@gmail.com>
2020-10-10 16:35:07 +0000

    updated files

--- src/main/java/com/cdmproject/helloworld/Helloworld.java
+++ src/main/java/com/cdmproject/helloworld/Helloworld.java
@@ -3,6 +3,7 @@ package com.cdmproject.helloworld;
 public class Helloworld {
 
 	public static void main(String[] args) {
+        System.out.println("This code is to test connectivity");
         System.out.println("Welcome to Devops learning!");
 	System.out.println("This is for Github-webhooks testing");
         
