1220e20ef1a89d302830167ed094fa0bfd0ef02f
Debajyoti <debajyotisarkar1198@gmail.com>
2020-10-28 16:40:48 +0000

    Updated the Helloworld.java file

--- src/main/java/com/cdmproject/helloworld/Helloworld.java
+++ src/main/java/com/cdmproject/helloworld/Helloworld.java
@@ -6,6 +6,7 @@ public class Helloworld {
         System.out.println("This code is to test connectivity");
         System.out.println("Welcome to Devops learning!");
 	System.out.println("This is for Github-webhooks testing");
+	System.out.println("Further write Ansible playbook for the same job");
         
 		}
 }
