3b09487bef3aa884818d5a6b9fab4c24a3469898
DebajyotiSar <76785790+DebajyotiSar@users.noreply.github.com>
2021-01-07 23:12:41 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Description: This is a sample test file
 #Created by : Debajyoti Sarkar
-#Date:04-12-2020
+#Date:07-01-2021
 #Place : Hyderabad
 
-echo "This is a Maven job project"
+echo "This is a AWS project"
 echo "Welcom to Devops Team in Hyderabad""
